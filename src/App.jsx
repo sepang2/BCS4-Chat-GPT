04df9434473090f857d5fe19358aea5adca2d132
Sepang2 <sepang_2@naver.com>
2023-11-15 15:51:56 +0900

    Chat bar setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+import ChatBar from "./components/ChatBar";
+
 const App = () => {
+  const [chatList, setChatList] = useState([]);
+
   return (
     <div className="min-h-screen max-w-screen-md mx-auto flex">
       <div className="bg-blue-100 w-52">Side</div>
       <div className="grow flex flex-col">
         <div className="bg-yellow-100 grow">Main</div>
-        <div className="bg-purple-100 h-24">Chat Bar</div>
+        <ChatBar chatList={chatList} setChatList={setChatList} />
       </div>
     </div>
   );
---
+++ src/components/ChatBar.jsx
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { useState } from "react";
+import { CgChevronUpO, CgSpinner } from "react-icons/cg";
+
+const ChatBar = ({ chatList, setChatList }) => {
+  const [newQuestion, setNewQuestion] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmitChat = async (e) => {
+    try {
+      e.preventDefault();
+      if (!newQuestion) return;
+
+      setIsLoading(true);
+
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: newQuestion,
+            },
+          ],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.REACT_APP_OPEN_AI_KEY}`,
+          },
+        }
+      );
+      setChatList([
+        ...chatList,
+        {
+          question: newQuestion,
+          answer: response.data.choices[0].message.content,
+        },
+      ]);
+      setNewQuestion("");
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="h-24">
+      <form className="h-full flex items-center px-4" onSubmit={onSubmitChat}>
+        <input
+          className={`mr-4 grow py-1 px-2 focus:outline-none border-2 focus:border-sky-500 ${
+            isLoading && "bg-gray-100 text-gray-500"
+          }`}
+          type="text"
+          value={newQuestion}
+          onChange={(e) => setNewQuestion(e.target.value)}
+          disabled={isLoading}
+          placeholder="Ask anything !"
+        />
+        <button
+          className="w-8 py-[6px] text-2xl bg-yellow-200 hover:bg-yellow-300 active:bg-yellow-200 rounded-full flex justify-center"
+          disabled={isLoading}
+        >
+          {isLoading ? (
+            <CgSpinner className="animate-spin-slow" />
+          ) : (
+            <CgChevronUpO />
+          )}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatBar;
